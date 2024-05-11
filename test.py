7c9fd76b0243579fb3fe99c388003b5d01f4df78
catel2 <156960644+catel2@users.noreply.github.com>
2024-05-11 16:03:32 -0500

    Update test.py

--- test.py
+++ test.py
@@ -17,3 +17,32 @@ def test_read_csv():
   songs_df = read_csv("~/Downloads/songs_normalize.csv")
   assert isinstance(songs_df, pd.DataFrame) #check
 
+def test_user_input(monkeypathc):
+  #testing user_input
+  #mocking user input with monkeypatch
+  mock_input_values = ['hip hop', '3.5', '6.3']
+  monkeypatch.setattr('builtins.input', lambda _: mock_input_values.pop(0))
+  genre, stride, speed = user_input()
+  assert isinstance(genre, str)
+  assert isinstance(stride, float)
+  assert isinstance(speed, float)
+
+def test_data_filter(sample_songs_df):
+  #testing data_filter
+  genre = 'hip hop'
+  stride = 3.5
+  speed = 6.3
+  filtered_df = data_filter(genre, stride, speed, sample_songs_df)
+  assert not filtered_df.empty
+
+def test_final_playlist(capsys):
+  #testing final_playlist
+  sample_playlist_df = pd.DataFrame({
+    'title': ['Song 1', 'Song 2'],
+    'artist': ['Artist 1', 'Artist 2']})
+  final_playlist(sample_playlist_df)
+  captured = capsys.readouterr()
+  assert "Here's your playlist!" in captured.out
+  assert "Song 1 by Artist 1" in captured.out
+  assert "Song 2 by Artist 2" in captured.out
+
